85e7ddc9719bc559957d4d66dfe321b3941b8c65
League Student <league@iMac-5.attlocal.net>
2019-10-17 18:28:24 -0700

    HappyPet :IP: :smile:

--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -5,26 +5,41 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	 static int happinessLevel = 0;
 	public static void main(String[] args) {
+		
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		int a = JOptionPane.showOptionDialog(null, "What kind of pet do you want", "question 1", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "Golden retriever", "White poodle", "Indian squater" }, null);
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to your puppy?", "Question 2", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "food", "groom", "Clean" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+				
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
 	}
-
+	public static void food() {
+		  JOptionPane.showMessageDialog(null, "The puppy likes the vegan food!");
+		  happinessLevel++;
+	}
+	public static void groom() {
+		JOptionPane.showMessageDialog(null, "The puppy is happy getting a grooming");
+		happinessLevel++;
+	}		
+	public static void clean(){
+		JOptionPane.showMessageDialog(null, "The puppy is thankfull for getting rid of its parasite infection");
+		happinessLevel++;
+	}
+	
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+			
 }
